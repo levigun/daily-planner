b5584addc2af228db717cd4b359d61a8d88d7906
levigun <t.levina31@gmail.com>
2022-01-07 22:32:30 +1100

    fixed timeblock colors according to time

--- assets/script.js
+++ assets/script.js
@@ -6,13 +6,12 @@ var toDoInput = document.querySelector(".todo-input");
 saveButton.addEventListener("click", function (event) {
     event.preventDefault();
     console.log(toDoInput.value)
-
-
+    
     // after an event has been entered, the save button is clicked
     // saved to local storage
-
+    
     localStorage.setItem('hour', JSON.stringify(toDoInput.value));
-
+    
 });
 
 
@@ -20,21 +19,26 @@ function saveEvent() {
     return localStorage.getItem('hour', JSON.parse((toDoInput.value)));
 }
 
+
 // when refreshed, the events are still there
 
 // when the date has changed, everything is gone
 // localStorage.clear();
 
+
 // add current day to header with moment.js
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
-var hour = $(".hour");
+
 
 // create current time var
 var currentHour = moment().format("h A");
-var splittedHour = hour.split();
-    console.log(hour)
+    console.log(currentHour);
+
+// create hour var from each timeblock
+var hour = document.getElementById("#9");
+    console.log(hour);  
 
 // div of each time block is compared to current time
 // when it is the current time, the box is red
@@ -43,16 +47,13 @@ if (currentHour == hour) {
 
     // when it is the time after, the box is green
 } else if (currentHour < hour) {
-
     $(".todo-input").addClass("future");
 
-} else {
-
     // when it is the time prior, the box is light gray
+} else {
     $(".todo-input").addClass("past");
 }
 
-// when the time is not shown in .hour, it will not do anything
 
-// when the day has changed, the whole page reloads and nothing is shw.
+// when the day has changed, the whole page reloads and nothing is shown.
 
