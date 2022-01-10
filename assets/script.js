759faca26cfee03b6cf2d240a386cfdd5e24cd0b
levigun <t.levina31@gmail.com>
2022-01-10 22:16:28 +1100

    code refactoring

--- assets/script.js
+++ assets/script.js
@@ -3,54 +3,52 @@ var saveButton = document.querySelectorAll(".save-button");
 var inputHour = $(".todo-input");
 var form = $("form");
 
-
+// created a function to get the user input from the local storage
 function getUserInputFromLocalStorage() {
+     // use the or || to make sure both are objects 
     return JSON.parse((localStorage.getItem("userInput"))) || {};
 }
 
+// created a function to get the text from each timeblock
 function getHourText(hour) {
-    const userInputs = getUserInputFromLocalStorage();
+    var userInputs = getUserInputFromLocalStorage();
     return userInputs['hour' + hour] || "";
 
 }
 
+// created a function to save the user input
 function saveInput(hour, content) {
-
-    // get the existing data
-    // use the or || to make sure both are objects 
-    // updated 
-
     console.log(hour);
+    // getting the user input
     var userInput = getUserInputFromLocalStorage();
     userInput['hour' + hour] = content;
 
-
-    // and saved again
-    // get all the value inputed in the timeblocks
-    // the written event stays even after other events are written
-    // save to local storage
+    // make the user input into a string
     var userInputSerialized = JSON.stringify(userInput);
+    // set to local storage
     localStorage.setItem("userInput", userInputSerialized);
 
-    var userInputSerialized = JSON.parse((localStorage.getItem("userInput")));
+    // get the item from local storage
+    var userInputSerialized = getUserInputFromLocalStorage();
     console.log(userInputSerialized);
 
-}
+};
 
+// for each every save button
 saveButton.forEach(function (saveButton) {
 
-    // put in the input valie
+    // put in the input value
     var textBox = saveButton.previousElementSibling;
 
-    // get the local storaeg
+    // get the local storage
     textBox.value = getHourText(textBox.id);;
 
 
-    //. event.target ==> the element that fired the event
-
-    // event.currentTarget is the element that u called the 'addEventListener'
-    // after an event has been entered, the save button is clicked
+    
+    // create a click event listener for the button
     saveButton.addEventListener("click", function (event) {
+        // event.target ==> the element that fired the event
+        // event.currentTarget is the element that u called the 'addEventListener'
         var element = event.currentTarget;
         var textBox = element.previousElementSibling
         // prevent default of reloading for the click event
@@ -58,6 +56,7 @@ saveButton.forEach(function (saveButton) {
         console.log(element);
         console.log(textBox);
 
+        // saving the user input in the textbox and stays in page
         var hour = $(textBox).attr('id');
         saveInput(hour, textBox.value);
 
