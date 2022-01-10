3903f4ed0526e63aad7944a165e03d62d8cee34f
levigun <t.levina31@gmail.com>
2022-01-10 14:16:51 +1100

    created functions for local storage and display

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,74 @@
 // Query Selectors
-var saveButton = document.querySelector(".save-button");
-var toDoInput = document.querySelector(".todo-input");
-
-// prevent default of reloading for the click event
-// TODO: forloop each button
-saveButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    console.log(toDoInput.value)
-
-    // after an event has been entered, the save button is clicked
-    // saved to local storage
-
-    localStorage.setItem('hour', JSON.stringify(toDoInput.value));
-
-});
+var saveButton = document.querySelectorAll(".save-button");
+var inputHour = $(".todo-input");
+var form = $("form");
 
 
-function saveEvent() {
-    return localStorage.getItem('hour', JSON.parse((toDoInput.value)));
+function getUserInputFromLocalStorage() {
+    return JSON.parse((localStorage.getItem("userInput"))) || {};
 }
 
-// const buttons = document.querySelectorAll(".save-button");
-// buttons.forEach(function(button){
-//   button.addEventListener("click", function(event){
-    // ...
-//   })
-// })
+function getHourText(hour) {
+    const userInputs = getUserInputFromLocalStorage();
+    return userInputs['hour' + hour] || "";
 
-// when refreshed, the events are still there
+}
 
-// when the date has changed, everything is gone
-// localStorage.clear();
+function saveInput(hour, content){
 
+    // get the existing data
+    // use the or || to make sure both are objects 
+    // updated 
+    
+    console.log(hour);
+    var userInput = getUserInputFromLocalStorage();
+    userInput['hour' + hour] = content;
+    
+    
+    // and saved again
+    
+    // get all the value inputed in the timeblocks
+    // the written event stays even after other events are written
+    
+    
+    // save to local storage
+    var userInputSerialized = JSON.stringify(userInput);
+    localStorage.setItem("userInput", userInputSerialized);
+    
+    var userInputSerialized = JSON.parse((localStorage.getItem("userInput")));
+    console.log(userInputSerialized);
+    
+    
+    // when the page reloads,
+    // the saved event is still displayed
+}
+
+saveButton.forEach(function (saveButton) {
+
+    // put in the input valie
+    var textBox = saveButton.previousElementSibling;
+
+    // get the local storaeg
+    textBox.value = getHourText(textBox.id);;
+
+
+    //. event.target ==> the element that fired the event
+
+    // event.currentTarget is the element that u called the 'addEventListener'
+    // after an event has been entered, the save button is clicked
+    saveButton.addEventListener("click", function (event) {
+        var element = event.currentTarget;
+        var textBox = element.previousElementSibling
+        // prevent default of reloading for the click event
+        event.preventDefault();
+        console.log(element);
+        console.log(textBox);
+
+        var hour = $(textBox).attr('id');
+        saveInput(hour, textBox.value);
+
+
+});
 
 // add current day to header with moment.js
 var today = moment();
@@ -41,10 +78,6 @@ $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 var currentHour = moment().hour();
 console.log(currentHour);
 
-// create input hour var from each timeblock
-var inputHour = $(".todo-input");
-console.log(inputHour);
-
 
 // for loop each inputhour with its length
 for (var i = 0; i < inputHour.length; i++) {
@@ -53,24 +86,24 @@ for (var i = 0; i < inputHour.length; i++) {
     // input hours index's id is compared to determine the time block current
     // when the time is the same, the block will be red
     if (currentHour == inputHour[i].id) {
-        console.log(currentHour); 
+        console.log(currentHour);
         console.log("id", inputHour[i].id);
-        $("#" + (inputHour[i].id)).addClass("present"); 
-    
-    // when the current hour is smaller value, the color is green
+        $("#" + (inputHour[i].id)).addClass("present");
+
+        // when the current hour is smaller value, the color is green
     } else if (currentHour < inputHour[i].id) {
-        console.log(currentHour); 
+        console.log(currentHour);
         console.log("id", inputHour[i].id);
         $("#" + (inputHour[i].id)).addClass("future");
 
-    // else the color will be light gray
+        // else the color will be light gray
     } else {
-        console.log(currentHour); 
+        console.log(currentHour);
         console.log("id", inputHour[i].id);
         $("#" + (inputHour[i].id)).addClass("past");
     }
 }
 
 
-// when the day has changed, the whole page reloads and nothing is shown.
 
+});
