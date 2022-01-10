4c934a8857d1c0210da7205bb5b20cc64fef2234
levigun <t.levina31@gmail.com>
2022-01-10 18:10:28 +1100

    added functions and local storage to work

--- assets/script.js
+++ assets/script.js
@@ -14,33 +14,27 @@ function getHourText(hour) {
 
 }
 
-function saveInput(hour, content){
+function saveInput(hour, content) {
 
     // get the existing data
     // use the or || to make sure both are objects 
     // updated 
-    
+
     console.log(hour);
     var userInput = getUserInputFromLocalStorage();
     userInput['hour' + hour] = content;
-    
-    
+
+
     // and saved again
-    
     // get all the value inputed in the timeblocks
     // the written event stays even after other events are written
-    
-    
     // save to local storage
     var userInputSerialized = JSON.stringify(userInput);
     localStorage.setItem("userInput", userInputSerialized);
-    
+
     var userInputSerialized = JSON.parse((localStorage.getItem("userInput")));
     console.log(userInputSerialized);
-    
-    
-    // when the page reloads,
-    // the saved event is still displayed
+
 }
 
 saveButton.forEach(function (saveButton) {
@@ -68,42 +62,39 @@ saveButton.forEach(function (saveButton) {
         saveInput(hour, textBox.value);
 
 
-});
+    });
 
-// add current day to header with moment.js
-var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
+    // add current day to header with moment.js
+    var today = moment();
+    $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
-// create current time var
-var currentHour = moment().hour();
-console.log(currentHour);
+    // create current time var
+    var currentHour = moment().hour();
+    console.log(currentHour);
 
 
-// for loop each inputhour with its length
-for (var i = 0; i < inputHour.length; i++) {
+    // for loop each inputhour with its length
+    for (var i = 0; i < inputHour.length; i++) {
 
-    // created an if condition
-    // input hours index's id is compared to determine the time block current
-    // when the time is the same, the block will be red
-    if (currentHour == inputHour[i].id) {
-        console.log(currentHour);
-        console.log("id", inputHour[i].id);
-        $("#" + (inputHour[i].id)).addClass("present");
+        // created an if condition
+        // input hours index's id is compared to determine the time block current
+        // when the time is the same, the block will be red
+        if (currentHour == inputHour[i].id) {
+            console.log(currentHour);
+            console.log("id", inputHour[i].id);
+            $("#" + (inputHour[i].id)).addClass("present");
 
-        // when the current hour is smaller value, the color is green
-    } else if (currentHour < inputHour[i].id) {
-        console.log(currentHour);
-        console.log("id", inputHour[i].id);
-        $("#" + (inputHour[i].id)).addClass("future");
+            // when the current hour is smaller value, the color is green
+        } else if (currentHour < inputHour[i].id) {
+            console.log(currentHour);
+            console.log("id", inputHour[i].id);
+            $("#" + (inputHour[i].id)).addClass("future");
 
-        // else the color will be light gray
-    } else {
-        console.log(currentHour);
-        console.log("id", inputHour[i].id);
-        $("#" + (inputHour[i].id)).addClass("past");
+            // else the color will be light gray
+        } else {
+            console.log(currentHour);
+            console.log("id", inputHour[i].id);
+            $("#" + (inputHour[i].id)).addClass("past");
+        }
     }
-}
-
-
-
 });
